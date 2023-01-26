d2e9593032b9d0ac85addbd0c58154b3f8c545ff
Maki <haru.maki.ch.0303@gmail.com>
2023-01-26 18:14:52 +0900

    add comment

--- script/setup_docker.sh
+++ script/setup_docker.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+# --------------------------
+# init
+#
 sudo apt update
 
-
+# --------------------------
+# docker
+#
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt update
+sudo apt install -y docker-ce
 
-sudo apt install docker-ce
-
+# --------------------------
+# docker-compose
+#
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
 sudo chmod +x /usr/local/bin/docker-compose
